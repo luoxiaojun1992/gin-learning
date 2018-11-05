c1ba7681b9c53901ae6937981f5159d83bc1589b
luoxiaojun <luoxiaojun@dodoca.com>
2018-11-05 19:17:48 +0800

    init commit

---
+++ init.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+if [ ! -e ./vendor ]; then
+	$GOPATH/bin/govendor init
+	$GOPATH/bin/govendor fetch github.com/gin-gonic/gin@v1.3
+	curl https://raw.githubusercontent.com/gin-gonic/gin/master/examples/basic/main.go > main.go
+fi
+
+go run main.go
